9421a21f99988576b571be31c9c8b95108ee54e0
Nightcoder <newnightcoder@gmail.com>
2021-09-15 07:15:46 +0200

    front connected to back :sunglasses:

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
+import cors from "cors";
 import express from "express";
-import { db } from "./DB/db.config.js";
 
 const app = express();
 const PORT = 3001;
 
-app.listen(PORT, () => {
-  console.log(`server's running on port ${PORT}`);
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+  console.log(req.body.userInput);
+  res.status(200).json({
+    message: "backend says hi!",
+  });
 });
 
-app.get("/", (req, res) => {
-  const testQuery = "USE redditDB";
-  db.getConnection((err, connexion) => {
-    if (err) {
-      console.log(err.message);
-      return;
-    }
-    console.log("connexion launched!");
-    connexion.query(testQuery, (err, result, fields) => {
-      connexion.release();
-      if (err) {
-        console.log("erreur test:", err);
-        return;
-      }
-      console.log("result!:", result);
-      console.log("fields!:", fields);
-      res.send("working like a charm!");
-    });
-  });
+// app.get("/", (req, res) => {
+//   const testQuery = "USE redditDB";
+//   db.getConnection((err, connexion) => {
+//     if (err) {
+//       console.log(err.message);
+//       return;
+//     }
+//     console.log("connexion launched!");
+//     connexion.query(testQuery, (err, result, fields) => {
+//       connexion.release();
+//       if (err) {
+//         console.log("erreur test:", err);
+//         return;
+//       }
+//       console.log("result!:", result);
+//       console.log("fields!:", fields);
+//       res.send("working like a charm!");
+//     });
+//   });
+// });
+
+app.listen(PORT, () => {
+  console.log(`server's running on port ${PORT}`);
 });

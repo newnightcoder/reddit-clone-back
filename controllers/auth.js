f5f529a2a01fe2ed450b092f90ec9223ebf8800e
Nightcoder <newnightcoder@gmail.com>
2021-09-19 01:47:27 +0200

    step2 signup implemented! (username/userId) :fire:

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,18 +17,26 @@ export const createUser = (req, res, next) => {
       throw err;
     }
 
-    const CREATE_USER = `INSERT INTO tbl_user (email, password) VALUES (${user.email}, ${user.password}) `;
+    const CREATE_USER = `
+    INSERT INTO tbl_user (email, password) 
+    VALUES (${user.email}, ${user.password})`;
 
     connection.query(CREATE_USER, (err, result, fields) => {
       connection.release();
       if (err) {
         console.log(err);
-        res.status(500).json({ errorMsg: err.message });
-        throw err;
+        const errorMsg = `Un compte avec cette adresse email existe déjà!\nSi vous êtes déjà membre, cliquez sur "se connecter" en bas de l'écran.`;
+        res.status(500).json({
+          errorMsg: err.errno === 1062 ? errorMsg : err.message,
+        });
+        return;
       }
-      console.log("SQL query result:", result);
+      console.log("SQL query result:", result.insertId);
       console.log("SQL query fields:", fields);
-      res.status(201).json({ message: "user added to DB!" });
+      res.status(201).json({
+        message: "user added to DB!",
+        userId: result.insertId,
+      });
       next();
     });
   });
@@ -57,25 +65,7 @@ export const logUser = (req, res, next) => {
     connection.query(FIND_USER, (err, result, fields) => {
       connection.release();
 
-      // switch ((err, result, fields)) {
-      //   case err:
-      //     res.status(404).json({
-      //       errorMsg: "Oops petite erreur serveur! Veuillez recommencer!",
-      //     });
-      //     break;
-      //   case result.length === 0:
-      //     res
-      //       .status(404)
-      //       .json({ errorMsg: "Pas de compte trouvé avec ces identifiants!" });
-      //     break;
-
-      //   default:
-      //     console.log("SQL query result:", result);
-      //     console.log("SQL query fields:", fields);
-      //     res.status(200).json({ message: "user in the DB! all good!" });
-      //     next();
-      //     break;
-      // }
+      console.log("SQL query result:", result);
 
       if (err) {
         console.log(err);
@@ -89,9 +79,58 @@ export const logUser = (req, res, next) => {
           .json({ errorMsg: "Pas de compte trouvé avec ces identifiants!" });
         return;
       }
-      console.log("SQL query result:", result);
-      console.log("SQL query fields:", fields);
       res.status(200).json({ message: "user in the DB! all good!" });
+      next();
+
+      // WITH SWITCH STATEMENT?
+      // switch ((err, result, fields)) {
+      //   case err:
+      //     res.status(404).json({
+      //       errorMsg: "Oops petite erreur serveur! Veuillez recommencer!",
+      //     });
+      //     break;
+      //   case result.length === 0:
+      //     res
+      //       .status(404)
+      //       .json({ errorMsg: "Pas de compte trouvé avec ces identifiants!" });
+      //     break;
+      //   default:
+      //     console.log("SQL query result:", result);
+      //     console.log("SQL query fields:", fields);
+      //     res.status(200).json({ message: "user in the DB! all good!" });
+      //     next();
+      //     break;
+      // }
+    });
+  });
+};
+
+export const addUserName = (req, res, next) => {
+  console.log("new username received!", req.body.userName);
+  console.log("id received!", typeof req.body.userId);
+
+  db.getConnection((err, connection) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({
+        errorMsg:
+          "Oops! petit problème de connexion. \n Veuillez rafraichir la page et recommencer.",
+      });
+      return;
+    }
+    const ADD_USERNAME = `
+    UPDATE tbl_user
+    SET username = ${JSON.stringify(req.body.userName)}
+    WHERE id = ${req.body.userId}
+    `;
+    connection.query(ADD_USERNAME, (err, result, fields) => {
+      connection.release();
+      if (err) {
+        console.log(err);
+        res.status(500).json({ errorMsg: err.message });
+        return;
+      }
+      res.status(200).json({ message: "username added successfully!" });
       next();
     });
   });
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,6 +3,7 @@ import * as auth from "../controllers/auth.js";
 const authRoute = express.Router();
 
 authRoute.post("/signup", auth.createUser);
+authRoute.post("/signup/username", auth.addUserName);
 authRoute.post("/login", auth.logUser);
 
 export default authRoute;

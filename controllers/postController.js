17236c1e233647cca8732ae5bd31476c4c517183
Nightcoder <newnightcoder@gmail.com>
2021-10-09 23:24:59 +0200

    post model under construction/getPosts+createPost ok! :construction:

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,61 +1,36 @@
-import { db } from "../DB/db.config.js";
+import { Post } from "../models/postModel.js";
 
 /////////////////////////////
 // GET ALL POSTS (FOR FEED)
 /////////////////////////////
 
 export const getPosts = async (req, res, next) => {
-  const GET_POSTS = `SELECT title, postId, text, date, fk_userId_post, username, picUrl FROM tbl_post, tbl_user WHERE tbl_post.fk_userId_post=tbl_user.id`;
-
-  const [posts, _] = await db.execute(GET_POSTS);
-
-  const postsInOrder = posts.sort((a, b) => {
-    if (a.postId < b.postId) return 1;
-    else return -1;
-  });
-  console.log(postsInOrder);
-  res.status(200).json({
-    posts: postsInOrder,
-  });
-  next();
+  try {
+    const posts = await Post.getPosts();
+    res.status(200).json({
+      posts,
+    });
+    next();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 ////////////////////
-// SAVE POST IN DB
+// CREATE POST IN DB
 ////////////////////
 
-export const createPost = (req, res, next) => {
-  const post = {
-    title: JSON.stringify(req.body.title),
-    text: JSON.stringify(req.body.text),
-    userId: req.body.userId,
-    date: JSON.stringify(req.body.date),
-  };
-
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.log(err.message);
-      return;
-    }
-
-    const CREATE_POST = `INSERT INTO tbl_post (title, text, fk_userId_post, date) VALUES (${post.title}, ${post.text},${post.userId},${post.date})`;
-    const GET_POST = `SELECT tbl_post.title, postId, text, date, fk_userId_post, username, picUrl FROM tbl_post, tbl_user WHERE tbl_post.fk_userId_post=tbl_user.id`;
-
-    connection.query(CREATE_POST, (err, result, fields) => {
-      connection.release();
-      if (err) {
-        console.log(err.message);
-        res.status(500).json({ errorMsg: "oops petit problèmepool!" });
-        return;
-      }
-      console.log("result:", result.insertId);
-      res.status(201).json({
-        message: "post créé et sauvé dans la DB!👍🏾",
-        // postId: result.insertId,
-      });
-      next();
-    });
-  });
+export const createPost = async (req, res, next) => {
+  const { userId, title, text, date } = req.body;
+  const post = new Post(null, userId, title, text, date, 0);
+  const errorDB = "Oops désolé, petit problème de post...";
+  try {
+    const postId = await post.create();
+    res.status(201).json({ postId });
+    next();
+  } catch (err) {
+    res.status(500).json({ error: errorDB });
+  }
 };
 
 ///////////////
---
+++ models/postModel.js
@@ -0,0 +1,39 @@
+import { db } from "../db/db.config.js";
+
+export class Post {
+  constructor(id, userId, title, text, date, likesCount) {
+    this.id = id;
+    this.userId = userId;
+    this.title = title;
+    this.text = text;
+    this.date = date;
+    this.likesCount = likesCount;
+  }
+
+  async create() {
+    const sqlCreatePost = `INSERT INTO tbl_post (fk_userId_post,title, text, date) VALUES (${this.userId},"${this.title}", "${this.text}","${this.date}")`;
+
+    try {
+      const [res, _] = await db.execute(sqlCreatePost);
+      const { insertId } = res;
+      return insertId;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async getPosts() {
+    const GET_POSTS = `SELECT title, postId, text, date, fk_userId_post, username, picUrl FROM tbl_post, tbl_user WHERE tbl_post.fk_userId_post=tbl_user.id`;
+    try {
+      const [posts, _] = await db.execute(GET_POSTS);
+
+      const postsInOrder = posts.sort((a, b) => {
+        if (a.postId < b.postId) return 1;
+        else return -1;
+      });
+      return postsInOrder;
+    } catch (error) {
+      throw error;
+    }
+  }
+}

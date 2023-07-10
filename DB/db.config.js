1c28001a6ff49c43133be789f24d375921070119
Nightcoder <newnightcoder@gmail.com>
2023-07-10 14:46:46 +0200

    DB hosting update to AWS :tada:

--- DB/db.config.js
+++ DB/db.config.js
@@ -2,18 +2,27 @@ import "dotenv/config";
 import mysql from "mysql2";
 
 // LOCAL DB:
+const dbConfigLocal = {
+  host: "localhost",
+  user: "root",
+  password: process.env.SQL_PASS_LOCAL,
+  database: "forum_backup",
+};
+
+// DB ON HEROKU / CLEARdb 😅
 // const dbConfig = {
-//   host: "localhost",
-//   user: "root",
-//   password: process.env.SQL_PASS,
-//   database: "redditDB",
+//   host: "eu-cdbr-west-02.cleardb.net",
+//   user: "b97aba0f0938eb",
+//   password: process.env.SQL_PASS_HEROKU,
+//   database: "heroku_3b1889097b1e2de",
 // };
 
+// DB ON AWS 🤩👌🏾
 const dbConfig = {
-  host: "eu-cdbr-west-02.cleardb.net",
-  user: "b97aba0f0938eb",
-  password: process.env.SQL_PASS,
-  database: "heroku_3b1889097b1e2de",
+  host: process.env.SQL_HOST_AWS,
+  user: "admin",
+  password: process.env.SQL_PASS_AWS,
+  database: "forumdb",
 };
 
 const pool = mysql.createPool(dbConfig);
